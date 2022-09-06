d3a322221db12c266d42db6a47f3029ea7ef4336
hlsmirn <hleb.smirnou@orioninc.com>
2022-09-06 11:04:16 +0300

    setup file edit

--- programStartSetup.py
+++ programStartSetup.py
@@ -4,7 +4,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 from constants import *
 
 driver = webdriver.Chrome(DRIVER_PATH)
-# driver.implicitly_wait(0)
 driver.get(MAIN_PAGE_LINK)
 wait = WebDriverWait(webdriver, 5)
 actions = ActionChains(driver)
